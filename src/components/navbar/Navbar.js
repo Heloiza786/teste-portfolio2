fe746255709ee3de02c8675bf2d7d2f20e7b1772
Heloiza786 <142845527+Heloiza786@users.noreply.github.com>
2023-09-05 18:32:26 -0300

    primeiro commit

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,25 @@
+
+//import './'
+
+function Navbar(){
+return( 
+    <div className="navbar">
+        <div> 
+         name
+            
+        </div>
+        
+         <div className="navbar-content">
+        
+          About
+          Tech 
+          Stack Projects Contact
+
+
+
+</div>
+    </div>
+);
+
+}
+export default Navbar;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+//import App from './components/app/App';
 import reportWebVitals from './reportWebVitals';
+import Navbar from './components/navbar/navbar'; 
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Navbar />
   </React.StrictMode>
+  
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
